18735d17827261061bedcb2772ffabc44a0ac4a9
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-02-26 11:57:18 -0300

    Update ifelse.js

--- ifelse.js
+++ ifelse.js
@@ -1,11 +1,13 @@
 //console.log("Hello Word!");
+//declaração de duas variaveis
+//o igual é um atribuidor
+var x = 2;
+var y = 2;
 
-var x = 1;
-var y = 1;
-
+// se a variavel x = y vai aparecer "Números conferem"
 if (x == y){
     console.log("Números Conferem!")
 
-} else {
+} else { // se as variaveis forem diferentes vai aparecer "Numeros divergem"
     console.log("Números Divergem!")
 }
